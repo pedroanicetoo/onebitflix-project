c79b5580f33c53d06f9ee567147b809a9d886df7
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2021-03-20 00:46:15 -0300

    Generate Models

--- onebitflix/db/schema.rb
+++ onebitflix/db/schema.rb
@@ -10,12 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_18_043200) do
+ActiveRecord::Schema.define(version: 2021_03_20_034237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.string "favoritable_type", null: false
+    t.bigint "favoritable_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["favoritable_type", "favoritable_id"], name: "index_favorites_on_favoritable_type_and_favoritable_id"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "thumbnail_key"
+    t.string "video_key"
+    t.boolean "highlighted", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "pg_search_documents", force: :cascade do |t|
     t.text "content"
@@ -24,6 +48,31 @@ ActiveRecord::Schema.define(version: 2021_03_18_043200) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "series", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "thumbnail_key"
+    t.boolean "highlighted", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "last_watched_episode_id"
+    t.index ["last_watched_episode_id"], name: "index_series_on_last_watched_episode_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +93,5 @@ ActiveRecord::Schema.define(version: 2021_03_18_043200) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "series", "movies", column: "last_watched_episode_id"
 end
